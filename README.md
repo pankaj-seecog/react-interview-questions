0098c1edf1d4d6fdbf061286591f3ceb5b1774cd
pankaj-seecog <pankaj.7613@gmail.com>
2020-10-18 21:11:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-interview-questions
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-interview-questions)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+import App2 from "./App2";
+export default function App() {
+  return (
+    <div>
+      <App2 />
+    </div>
+  );
+}
